847b1214bde806d4be5839e30ee8411275c7cfd9
nicolasprothero <nprothero0917@gmail.com>
2026-09-02 20:08:52 -0400

    update Div and Flex

--- src/Div/Div.tsx
+++ src/Div/Div.tsx
@@ -7,9 +7,19 @@ export interface DivProps extends React.HTMLAttributes<HTMLDivElement> {
   paddingY?: string | number;
   paddingX?: string | number;
   margin?: string | number;
+  marginX?: string | number;
+  marginY?: string | number;
   width?: string | number;
   height?: string | number;
   borderRadius?: string | number;
+  marginTop?: string | number;
+  marginRight?: string | number;
+  marginBottom?: string | number;
+  marginLeft?: string | number;
+  paddingTop?: string | number;
+  paddingRight?: string | number;
+  paddingBottom?: string | number;
+  paddingLeft?: string | number;
 }
 
 export const Div = ({
@@ -18,7 +28,17 @@ export const Div = ({
   padding,
   paddingY,
   paddingX,
+  paddingTop,
+  paddingRight,
+  paddingBottom,
+  paddingLeft,
   margin,
+  marginX,
+  marginY,
+  marginTop,
+  marginRight,
+  marginBottom,
+  marginLeft,
   width,
   height,
   borderRadius,
@@ -34,11 +54,15 @@ export const Div = ({
         boxSizing: "border-box",
         color,
         padding: padding,
-        paddingTop: paddingY,
-        paddingBottom: paddingY,
-        paddingLeft: paddingX,
-        paddingRight: paddingX,
-        margin,
+        paddingTop: paddingTop ?? paddingY,
+        paddingBottom: paddingBottom ?? paddingY,
+        paddingLeft: paddingLeft ?? paddingX,
+        paddingRight: paddingRight ?? paddingX,
+        margin: margin,
+        marginLeft: marginLeft ?? marginX,
+        marginRight: marginRight ?? marginX,
+        marginTop: marginTop ?? marginY,
+        marginBottom: marginBottom ?? marginY,
         width,
         height,
         borderRadius,
